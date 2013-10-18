30a6f923b08357851b10d1d9614d8b7911263afc
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-18 18:58:04 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wopu::Application.config.secret_key_base = '2e849dae1587214da69979d1339dcf099e5f4ed2c2800f7dfb5aa7648798569e8c083a72671145f2c1fb3740c643deffe76ccbedfa91e43738d7f17614e32cd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wopu::Application.config.session_store :cookie_store, key: '_wopu_session'
