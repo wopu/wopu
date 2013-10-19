3e772c8b1718ccf2b7ae454f9604bb2e62e9f9ef
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-19 11:22:57 -0500

    Remove comments in helps controller.

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,28 +1,20 @@
 class HelpsController < ApplicationController
   before_action :set_help, only: [:show, :edit, :update, :destroy]
 
-  # GET /helps
-  # GET /helps.json
   def index
     @helps = Help.all
   end
 
-  # GET /helps/1
-  # GET /helps/1.json
   def show
   end
 
-  # GET /helps/new
   def new
     @help = Help.new
   end
 
-  # GET /helps/1/edit
   def edit
   end
 
-  # POST /helps
-  # POST /helps.json
   def create
     @help = Help.new(help_params)
 
@@ -37,8 +29,6 @@ class HelpsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /helps/1
-  # PATCH/PUT /helps/1.json
   def update
     respond_to do |format|
       if @help.update(help_params)
@@ -51,8 +41,6 @@ class HelpsController < ApplicationController
     end
   end
 
-  # DELETE /helps/1
-  # DELETE /helps/1.json
   def destroy
     @help.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class HelpsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_help
       @help = Help.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def help_params
       params.require(:help).permit(:description)
     end
