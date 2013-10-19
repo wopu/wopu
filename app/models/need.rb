0bf290ff8a38063cace492f14f225d5f816b50bf
Victor Cortes <ie.cortex@gmail.com>
2013-10-19 14:14:36 -0500

    Add comments

--- app/models/need.rb
+++ app/models/need.rb
@@ -5,6 +5,9 @@ class Need
   field :title
   field :description, type: String
   field :status # Open, close
+  # field :category ??? Money, time. knowledge, food
+  # field :recurrent ??? Always exists? e.g. Money
+  # See what happened after the need was marked as close? Show results?
 
   belongs_to :foundation
   has_many :helps
