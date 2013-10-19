7f934a3a485b1d13d207ea3ff7213f21a1ff3eda
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-18 19:25:00 -0500

    deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,92 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# This capistrano deployment recipe is made to work with the optional
+# StackScript provided to all Rails Rumble teams in their Linode dashboard.
+#
+# After setting up your Linode with the provided StackScript, configuring
+# your Rails app to use your GitHub repository, and copying your deploy
+# key from your server's ~/.ssh/github-deploy-key.pub to your GitHub
+# repository's Admin / Deploy Keys section, you can configure your Rails
+# app to use this deployment recipe by doing the following:
+#
+# 1. Add `gem 'capistrano', '~> 2.15'` to your Gemfile.
+# 2. Run `bundle install --binstubs --path=vendor/bundles`.
+# 3. Run `bin/capify .` in your app's root directory.
+# 4. Replace your new config/deploy.rb with this file's contents.
+# 5. Configure the two parameters in the Configuration section below.
+# 6. Run `git commit -a -m "Configured capistrano deployments."`.
+# 7. Run `git push origin master`.
+# 8. Run `bin/cap deploy:setup`.
+# 9. Run `bin/cap deploy:migrations` or `bin/cap deploy`.
+#
+# Note: You may also need to add your local system's public key to
+# your GitHub repository's Admin / Deploy Keys area.
+#
+# Note: When deploying, you'll be asked to enter your server's root
+# password. To configure password-less deployments, see below.
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#############################################
+##                                         ##
+##              Configuration              ##
+##                                         ##
+#############################################
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+GITHUB_REPOSITORY_NAME = 'r13-team-135'
+LINODE_SERVER_HOSTNAME = '50.116.4.94'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+#############################################
+#############################################
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# General Options
+
+set :bundle_flags,               "--deployment"
+
+set :application,                "railsrumble"
+set :deploy_to,                  "/var/www/apps/railsrumble"
+set :normalize_asset_timestamps, false
+set :rails_env,                  "production"
+
+set :user,                       "root"
+set :runner,                     "www-data"
+set :admin_runner,               "www-data"
+
+# Password-less Deploys (Optional)
+#
+# 1. Locate your local public SSH key file. (Usually ~/.ssh/id_rsa.pub)
+# 2. Execute the following locally: (You'll need your Linode server's root password.)
+#
+#    cat ~/.ssh/id_rsa.pub | ssh root@LINODE_SERVER_HOSTNAME "cat >> ~/.ssh/authorized_keys"
+#
+# 3. Uncomment the below ssh_options[:keys] line in this file.
+#
+ssh_options[:keys] = ["~/.ssh/id_rsa"]
+
+# SCM Options
+set :scm,        :git
+set :repository, "git@github.com:railsrumble/#{GITHUB_REPOSITORY_NAME}.git"
+set :branch,     "master"
+
+# Roles
+role :app, LINODE_SERVER_HOSTNAME
+role :db,  LINODE_SERVER_HOSTNAME, :primary => true
+
+# Add Configuration Files & Compile Assets
+after 'deploy:update_code' do
+  # Setup Configuration
+  run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
+  # Compile Assets
+  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+
+# Restart Passenger
+deploy.task :restart, :roles => :app do
+  # Fix Permissions
+  sudo "chown -R www-data:www-data #{current_path}"
+  sudo "chown -R www-data:www-data #{latest_release}"
+  sudo "chown -R www-data:www-data #{shared_path}/bundle"
+  sudo "chown -R www-data:www-data #{shared_path}/log"
+
+  # Restart Application
+  run "touch #{current_path}/tmp/restart.txt"
+end
