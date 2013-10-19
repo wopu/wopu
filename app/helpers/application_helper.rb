bc2f270b8629aec1aae23506e0795ffbb723c609
Victor Cortes <ie.cortex@gmail.com>
2013-10-19 12:19:14 -0500

    Add error messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def error_messages(resource)
+    return unless resource.errors.any?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+      <div class="error-explanation">
+        <ul>#{messages}</ul>
+      </div>
+    HTML
+
+    html.html_safe
+  end
 end
