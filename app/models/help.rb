68b655dbabd5fad66fcb49def1e15eeb051498e1
Victor Cortes <ie.cortex@gmail.com>
2013-10-19 14:29:01 -0500

    Show the need and helps

--- app/models/help.rb
+++ app/models/help.rb
@@ -3,7 +3,7 @@ class Help
   include Mongoid::Timestamps
 
   field :description, type: String
-  field :status # Pending, accepted, requested
+  field :status # Pending, accepted, requested, verified/closed/received
 
   belongs_to :need
   belongs_to :helper, polymorphic: true
