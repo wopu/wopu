0d617726597d88bff61164064c44d723b53a0141
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-20 12:06:35 -0500

    Add seeds and sample data.

---
+++ db/sample_data.rb
@@ -0,0 +1,45 @@
+
+Mongoid.raise_not_found_error = false
+
+def category; Category.all.sample end
+def available_tag_ids; AvailableTag.all.sample(2).map(&:id) end
+
+password = '123123'
+
+unless user = User.find_by(name: 'test')
+  user = User.create(name: 'test', email: 'test@test.com', password: password)
+end
+
+if user.foundations.empty?
+  3.times do
+    Foundation.create category: category, name: Faker::Company.name,
+      available_tag_ids: available_tag_ids
+  end
+end
+
+4.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email,
+    password: password)
+end
+
+3.times do
+
+  foundation = Foundation.find_by(name: Faker::Company.name)
+
+  unless foundation
+    foundation = Foundation.create(category: category, name: Faker::Company.name,
+      available_tag_ids: available_tag_ids)
+  end
+
+  if foundation.needs.empty?
+    3.times do
+      need = foundation.needs.create description: Faker::Lorem.sentence
+
+      3.times do
+        need.helps.create description: Faker::Lorem.sentence,
+          helper: User.all.sample
+      end
+    end
+  end
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Mongoid.raise_not_found_error = false
+
+%w( asd qwe tre gfd ).each do |name|
+  unless AvailableTag.find_by name: name
+    AvailableTag.create name: name
+  end
+end
+
+%w( dsa ewq ).each do |name|
+  unless Category.find_by name: name
+    Category.create name: name
+  end
+end
