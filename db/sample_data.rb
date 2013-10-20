0a1e2040a8d824bf58e03557507726a761984289
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-20 12:44:02 -0500

    Add dots.

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -6,22 +6,28 @@ def available_tag_ids; AvailableTag.all.sample(2).map(&:id) end
 
 password = '123123'
 
+puts "\nCreating test user and foundations for this user"
 unless user = User.find_by(name: 'test')
   user = User.create(name: 'test', email: 'test@test.com', password: password)
+  print '.'
 end
 
 if user.foundations.empty?
   3.times do
     Foundation.create category: category, name: Faker::Company.name,
       available_tag_ids: available_tag_ids
+    print '.'
   end
 end
 
+puts "\nCreating users"
 4.times do
   User.create(name: Faker::Name.name, email: Faker::Internet.email,
     password: password)
+  print '.'
 end
 
+puts "\nCreating foundations, needs and helps"
 3.times do
 
   foundation = Foundation.find_by(name: Faker::Company.name)
@@ -29,15 +35,18 @@ end
   unless foundation
     foundation = Foundation.create(category: category, name: Faker::Company.name,
       available_tag_ids: available_tag_ids)
+    print 'f'
   end
 
   if foundation.needs.empty?
     3.times do
       need = foundation.needs.create description: Faker::Lorem.sentence
+      print 'n'
 
       3.times do
         need.helps.create description: Faker::Lorem.sentence,
           helper: User.all.sample
+        print '.'
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,18 @@
 
 Mongoid.raise_not_found_error = false
 
+puts "\nCreating AvailableTags"
 %w( asd qwe tre gfd ).each do |name|
   unless AvailableTag.find_by name: name
     AvailableTag.create name: name
   end
+  print '.'
 end
 
+puts "\nCreating Categories"
 %w( dsa ewq ).each do |name|
   unless Category.find_by name: name
     Category.create name: name
   end
+  print '.'
 end
