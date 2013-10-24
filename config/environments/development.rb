fc3653a377942a58001b8adcfc880135def193db
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-10-24 17:25:46 -0500

    Add letter_opener.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,5 +24,7 @@ Wopu::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.delivery_method = :letter_opener
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
