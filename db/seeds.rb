e7d03ee7619faffa2b4895faebd4ff41e5c8707b
Victor Cortes <ie.cortex@gmail.com>
2013-11-01 11:58:48 -0500

    Delete sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-
-Mongoid.raise_not_found_error = false
-
-puts "Creating AvailableTags"
-%w( study food recreation ).each do |name|
-  unless AvailableTag.find_by name: name
-    AvailableTag.create name: name
-  end
-  print '.'
-end
-puts ''
-
-puts "Creating Categories"
-%w( Children Health Nutrition ).each do |name|
-  unless Category.find_by name: name
-    Category.create name: name
-  end
-  print '.'
-end
-puts ''
--- lib/tasks/sample_data.rake
+++
@@ -1,9 +0,0 @@
-namespace :db do
-  desc 'Create sample data'
-  task sample_data: :environment do
-    load Rails.root.join 'db', 'sample_data.rb'
-  end
-
-  desc 'Seed and sample DB'
-  task bootstrap: [:drop, :seed, :sample_data]
-end
