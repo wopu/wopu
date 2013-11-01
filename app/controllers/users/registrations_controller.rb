208ba4ad7edd4e5a27094fd172f6a6ddd0af7238
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-11-01 00:05:14 -0500

    Registrations working.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+      end
+    else
+      flash[:notice] = (( resource.errors.full_messages +
+       [ "Please try again" ] ).uniq.join('.<br/>') + '.').html_safe
+      clean_up_passwords resource
+    end
+    redirect_to root_path
+  end
+
   def resource_params
     params.require(:user).permit(:name, :avatar, :email, :password, :password_confirmation)
   end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -13,6 +13,34 @@ describe 'Sign up modal' do
       find('input[type=submit]').click
       User.all.size.should == old_count + 1
     end
+
+    visit '/'
+    click_link 'Sign up'
+    old_count = User.all.size
+    within '#userModal' do
+      fill_in 'user[name]', with: 'some'
+      fill_in 'user[email]', with: 'some@test.com'
+      fill_in 'user[password]', with: '123123123'
+      fill_in 'user[password_confirmation]', with: '123123123'
+      find('input[type=submit]').click
+      User.all.size.should == old_count
+    end
+
+    current_path.should == '/'
+
+    visit '/'
+    click_link 'Sign up'
+    old_count = User.all.size
+    within '#userModal' do
+      fill_in 'user[name]', with: 'some'
+      fill_in 'user[email]', with: 'some@test.com'
+      fill_in 'user[password]', with: '123123123'
+      fill_in 'user[password_confirmation]', with: '123123123'
+      find('input[type=submit]').click
+      User.all.size.should == old_count + 1
+    end
+
+
   end
 end
 
